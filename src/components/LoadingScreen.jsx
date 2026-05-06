f90de4e0b07327909621c29e58cd8282a3e54a5d
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-05-06 13:34:05 -0300

    feat: add LoadingScreen component structure

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,15 @@
+export default function LoadingScreen() {
+  return (
+    <div className="loader">
+      <div className="loader__bar-track">
+        <div className="loader__bar-fill" />
+      </div>
+
+      <div className="loader__center">
+        <span>LUCAS</span>
+        <span>SILVA</span>
+        <div>SOFTWARE ENGINEER</div>
+      </div>
+    </div>
+  );
+}
